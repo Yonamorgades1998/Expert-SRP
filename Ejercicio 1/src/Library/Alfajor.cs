b81c1ae93eef5c9906cb44f6fb961f74f3ba63cf
Yonamorgades1998 <yonatan.morgades@correo.ucu.edu.uy>
2020-09-01 19:49:06 -0300

    Se suben los comentarios de SRP y Expert

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -3,7 +3,8 @@ using System;
 namespace Expert_SRP
 {
     public class Alfajor 
-    {   //Cumple con el patros SRP
+    {
+         //Cumple con el patros SRP
         // Esta clase cumple con el principio expert ya que conoce toda la información de Alfajor y todas sus funcionalidades        Esta clase cumple con el principio expert ya que conoce toda la informacion de Alfajor y todas sus funcionalidades
         public Alfajor(double precioMasa, double precioDulce)
         {
