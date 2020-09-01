17f8b00c8c00f246a3968df0df68d81dc023a78e
Yonamorgades1998 <yonatan.morgades@correo.ucu.edu.uy>
2020-09-01 19:47:21 -0300

    First commit

---
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Expert_SRP
+{
+    public class Alfajor 
+    {   //Cumple con el patros SRP
+        // Esta clase cumple con el principio expert ya que conoce toda la información de Alfajor y todas sus funcionalidades        Esta clase cumple con el principio expert ya que conoce toda la informacion de Alfajor y todas sus funcionalidades
+        public Alfajor(double precioMasa, double precioDulce)
+        {
+            this.PrecioDulce = precioDulce;
+            this.PrecioMasa = precioMasa;
+        }
+        public Double PrecioDulce {get;set;}
+        public Double PrecioMasa {get; set;}
+    }
+}
---
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Expert_SRP
+{
+    public class Kiosco 
+    {
+        // Esta función no cumple el patron de SRP ya que tiene una funcion de calcular  una conversion que podria ser  de otra clase
+        public Boolean PuedeComprar(Alfajor a, Double dinero, String moneda) 
+        {
+            Double pesos = ConvertirAPesos(dinero, moneda);
+            return pesos >= a.PrecioDulce + a.PrecioMasa;
+        }
+        private Double ConvertirAPesos(Double dinero, String moneda)
+        {
+            if (moneda.Equals("U$S")) 
+            {
+                return dinero / 30;
+            } 
+            else if (moneda.Equals("$")) 
+            {
+                return dinero;
+            }
+            else 
+            {
+                return -1;
+            }
+        }
+    }
+}
